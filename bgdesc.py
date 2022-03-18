e30cafd13dd37c459952f167006f828fffdd2423
Cole Zlomke <colezlomke@gmail.com>
2022-03-18 15:53:44 -0500

    3/18

--- bgdesc.py
+++ bgdesc.py
@@ -1,3 +1,4 @@
+from ast import Str
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -9,13 +10,14 @@ soup = BeautifulSoup(req.text, "html.parser")
 
 #find all background related information
 array = soup.find_all('a',href=re.compile("background"))
-length = len(array)-3
+length = len(array)-90 #change this back to -50 if all, 93 if only acolyte
 isvariant = ["Investigator (SCAG)", "Investigator (VRGR)", "Spy", "Gladiator", "Guild Merchant", "Knight", "Pirate"]
 hasvariant = ["City Watch", "Criminal", "Entertainer", "Guild Artisan", "Noble", "Sailor"]
 notoc = ["Shipwright", "Smuggler"]
-keywords = ["Features", "Variants"]
+keywords = ["Features", "Variants", "Suggested Characteristics"]
 #begin loop
 i = 0
+backgrounds = []
 while i < length:
     units = array[i]
     refs = units['href']
@@ -23,10 +25,6 @@ while i < length:
     if bgs in isvariant:
         i += 1
         continue
-    #stop when it reaches black fist double agent, all backgrounds following are not ones I want to include, also too many edge cases
-    if refs == "/background:black-fist-double-agent":
-        i += 1
-        break
     #enters secondary url
     urlbgs = (url + refs) 
     reqbgs = requests.get(urlbgs)
@@ -35,8 +33,8 @@ while i < length:
     #finds the correct text block background description 1
     textbgs = soupbgs.find(text=re.compile('Skill'))
     bgblock1 = textbgs.parent.parent
-    bgs2 = bgblock1.get_text()
-
+    bgs2 = bgblock1.get_text(strip=True)
+    #
     #removes the hover text
     nohover = bgblock1.find_all(text=re.compile('Value:'))
     hovlen = len(nohover)
@@ -47,28 +45,62 @@ while i < length:
         i2+=1
     
     #begin background description section 2 parsing
-    print(bgs)
-    #print(bgs2)
+
     toc0 = soupbgs.find(id=re.compile('toc0'))
     toc1 = soupbgs.find(id=re.compile('toc1'))
     toc2 = soupbgs.find(id=re.compile('toc2'))
-    print(toc0.get_text())
+    #######################################################
+    toc0text = toc0.get_text()
+    toc1text = toc1.get_text()
+    toc2text = toc2.get_text()
+    #######################################################
+    toc0more = ""
+    toc1more = ""
+    toc2more = ""
+    #######################################################
+    toc0more2 = ""
+    toc1more2 = ""
+    toc2more2 = ""
+    #######################################################
+    if toc0text not in keywords:
+        toc0more=(toc0.next_sibling.next_sibling.get_text(strip=True))
+    else:
+        toc1text = ""
+        toc0more = ""
+    if toc1text not in keywords:
+        toc1more=(toc1.next_sibling.next_sibling.get_text(strip=True))
+    else:
+        toc1text = ""
+        toc1more = ""
+    if toc2text not in keywords:
+        toc2more=(toc2.next_sibling.next_sibling.get_text(strip=True))
+    else:
+        toc2text = ""
+        toc2more = ""
+    ########################################################
+    toc0more2=(toc0.next_sibling.next_sibling.next_sibling.get_text(strip=True))
+    if toc0more2 in keywords:
+        toc0more2 = ""
+    toc1more2=(toc1.next_sibling.next_sibling.next_sibling.get_text(strip=True))
+    if toc1more2 in keywords:
+        toc1more2 = ""
+    toc2more2=(toc2.next_sibling.next_sibling.next_sibling.get_text(strip=True))
+    if toc2more2 in keywords:
+        toc2more2 = ""
+    #######################################################
 
-    if toc0.get_text() not in keywords:
-        print(toc0.next_sibling.next_sibling.get_text())
+    #
+    fullbgdesc = (toc0text +" "+ toc0more +" "+ toc0more2 +" "+ toc1text +" "+ toc1more +" "+ toc1more2 +" "+ toc2text +" "+ toc2more  +" "+ toc2more2)
+    backgrounds.append({
+            'Background': bgs,
+            'Proficiencies': bgs2,
+            'Description': fullbgdesc,
+    })
+    #
     
-    print(toc1.get_text())
-    print(toc1.find_next_sibling('p').get_text())
-    toc1check = toc1.find_next_sibling('p').next_sibling.next_sibling.get_text()
-    if toc1check != 'Suggested Characteristics':
-        print(toc1check)
-    if toc2.get_text() == 'Suggested Characteristics':
-        i+=1
-        continue
-    print(toc2.get_text())
-
     #print(bgs4)
     i += 1
+print(backgrounds)
 
 
 
